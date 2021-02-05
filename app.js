860f9ae78256c7acfa63119d1111b535422e199f
AzeemWahid <67079302+AzeemWahid@users.noreply.github.com>
2021-02-05 19:25:07 -0400

    mongodb linked

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = 3000;
+const app = express();
 
-var app = express();
+// define routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const patientsRouter = require('./routes/patients');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//register modules
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// register routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +43,17 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+//connect to db and listen
+app.listen(port, () => {
+  mongoose.connect('mongodb+srv://MAL_DbUser:physicaltherapydb1@pt-app.5pwkv.mongodb.net/PTdb?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log("connected to DB!");
+  });
+  console.log(`PT app listening at http://localhost:${port}`)
 });
 
 module.exports = app;
